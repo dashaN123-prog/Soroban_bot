fb0f270bc649cdb8df3c01ab718839ad218bbb59
dashaN123-prog <dasanovak542@gmail.com>
2025-07-22 02:22:53 +0300

    ыфвфыв

--- keyboards/success_kb.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-success_back_kb = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="🔙 Назад", callback_data="back_to_start")]
-])
